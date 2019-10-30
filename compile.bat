ba13770e60abd76e6d5563c5d70810b59d3d40b2
John Benedick Estrada <estradaben8@gmail.com>
2019-10-30 23:07:29 +0800

    Migrated `SaveFormat` mod to save.f08

--- compile.bat
+++ compile.bat
@@ -7,4 +7,6 @@ gfortran -c population.f08 -o ./obj/a5.o
 gfortran -c stat.f08 -o ./obj/a6.o 
 gfortran -c update_array.f08 -o ./obj/a7.o 
 gfortran -c ticker.f08 -o ./obj/a8.o 
-gfortran ./obj/a1.o ./obj/a2.o ./obj/a3.o ./obj/a4.o ./obj/a5.o ./obj/a6.o ./obj/a7.o ./obj/a8.o -o penna.exe
+gfortran -c save.f08 -o ./obj/a9.o 
+gfortran ./obj/a1.o ./obj/a2.o ./obj/a3.o ./obj/a4.o ./obj/a5.o ./obj/a6.o^
+ ./obj/a7.o ./obj/a8.o ./obj/a9.o -o penna.exe
