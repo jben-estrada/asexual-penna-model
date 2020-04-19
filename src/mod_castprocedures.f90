48a5ea2f8e5c821d005a2a58c2724e85387460dd
John Benedick Estrada <estradaben8@gmail.com>
2020-04-20 00:00:48 +0800

    Added real-to-char and char-to-real cast funcs.

--- src/mod_castprocedures.f90
+++ src/mod_castprocedures.f90
@@ -13,9 +13,15 @@ module CastProcedures
   integer, parameter :: MAX_LEN = 64
     !! Maximum character length for x-to-char procedures.
 
+  ! Default read/write formats.
+  character(len=*), parameter :: DEF_INT_FORMAT = "(i10)"
+  character(len=*), parameter :: DEF_REAL_FORMAT = "(f10.5)"
+
   public :: castCharToInt
   public :: castIntToChar
   public :: castIntPtrToChar
+  public :: castRealToChar
+  public :: castCharToReal
 contains
 
 
@@ -27,17 +33,16 @@ contains
     character(len=*), intent(in) :: char
       !! Character to cast to integer.
     integer, optional, intent(out) :: castStat
-    !! Casting status. A value of 0 means
+    !! Casting status.
 
     integer :: status
-    read(char, *, iostat=status) castCharToInt
+    read(char, DEF_INT_FORMAT, iostat=status) castCharToInt
 
-    ! Handle casting error in other procedures/program.
+    ! Let other routines to handle casting errors.
     if (present(castStat)) then
       castStat = status
     else
-
-      ! Handle casting error in this function.
+      ! By default, stop the program if casting failed.
       if (status /= 0) &
         call raiseError("'" // trim(adjustl(char)) // "' is not numeric.")
     end if
@@ -52,20 +57,20 @@ contains
     integer,           intent(in) :: int
       !! Integer to cast to character.
     integer, optional, intent(out) :: castStat
-      !! Casting status. A value of 0 means
+      !! Casting status.
 
     integer :: status
 
     ! Initialize the output.
     castIntToChar = ""
 
-    write(castIntToChar, *, iostat=status) int
+    write(castIntToChar, DEF_INT_FORMAT, iostat=status) int
 
-    ! Handle casting error in other procedures/program.
+    ! Let other routines to handle casting errors.
     if (present(castStat)) then
       castStat = status
     else
-      ! Handle casting error in this function.
+      ! By default, stop the program if casting failed.
       if (status /= 0) call raiseError("Casting from int to char failed.")
     end if
   end function castIntToChar
@@ -79,7 +84,7 @@ contains
     integer, pointer,  intent(in) :: intPtr
       !! Integer to cast to character.
     integer, optional, intent(out) :: castStat
-      !! Casting status. A value of 0 means
+      !! Casting status.
 
     integer :: status
 
@@ -87,21 +92,65 @@ contains
       ! Initialize the output.
       castIntPtrToChar = ""
 
-      write(castIntPtrToChar, *, iostat=status) intPtr
+      write(castIntPtrToChar, DEF_INT_FORMAT, iostat=status) intPtr
     else
       call raiseError("The 'int' dummy argument is not associated " // &
           "with any target.")
     end if
 
-    ! Handle casting error in other procedures/program.
+    ! Let other routines to handle casting errors.
     if (present(castStat)) then
       castStat = status
     else
-      
-      ! Handle casting error in this function.
-      if (status /= 0) then
-        call raiseError("Casting from int pointer to char failed.")
-      end if
+      ! By default, stop the program if casting failed.
+      if (status /= 0) &
+          call raiseError("Casting from int pointer to char failed.")
     end if
   end function castIntPtrToChar
+
+
+  ! -------------------------------------------------------------------------- !
+  ! FUNCTION: castCharToReal
+  !>  Cast the character `char` into a real number of default kind.
+  ! -------------------------------------------------------------------------- !
+  real function castCharToReal(char, castStat)
+    character(len=*), intent(in) :: char
+      !! Character input to be casted to real.
+    integer, optional, intent(out) :: castStat
+      !! Casting status.
+    
+    integer :: status
+    read(char, DEF_REAL_FORMAT, iostat=status) castCharToReal
+
+    ! Let other routines to handle casting errors.
+    if (present(castStat)) then
+      castStat = status
+    else
+      ! By default, stop the program if casting failed.
+      if (castStat /= 0) &
+          call raiseError("'" // trim(char) // "' is not numeric.")
+    end if
+  end function castCharToReal
+
+
+  ! -------------------------------------------------------------------------- !
+  ! FUNCTION: castRealToChar
+  !>  Cast the real number `realNum` into a 64 long character.
+  ! -------------------------------------------------------------------------- !
+  character(len=MAX_LEN) function castRealToChar(realNum, castStat)
+    real,              intent(in) :: realNum
+      !! Real number to cast to character.
+    integer, optional, intent(out) :: castStat
+    
+    integer :: status
+    write(castRealToChar, DEF_REAL_FORMAT, iostat=status) realNum
+
+    ! Let other routines to handle casting errors.
+    if (present(castStat)) then
+      castStat = status
+    else
+      ! By default, stop the program if casting failed.
+      if (status /= 0) call raiseError("Casting real to char failed.")
+    end if
+  end function castRealToChar
 end module CastProcedures
