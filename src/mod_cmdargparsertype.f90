ce7d7806756624a4385f681eff97853a1335b017
John Benedick Estrada <estradaben8@gmail.com>
2020-04-30 03:48:04 +0800

    Added a new and simpler cmd arg parser

---
+++ src/mod_cmdargparsertype.f90
@@ -0,0 +1,474 @@
+module CmdArgParserType
+  ! ------------------------------------------------------------------------- !
+  ! MODULE: CmdArgParserType
+  ! ------------------------------------------------------------------------- !
+  ! DESCRIPTION:
+  !>  Module containing the derived type `CmdArgParser` for parsing command
+  !!  arguments.
+  ! ------------------------------------------------------------------------- !
+  use HashTableType, only: &
+    HashTable, &
+    HashTableIterator, &
+    HSHTBLE_STAT_OK => STAT_OK
+  use ErrorMSG, only: raiseError, raiseWarning
+  implicit none
+  private
+
+  ! Command types.
+  character, parameter :: CMD_TYPE_FLAG_S = "f"
+    !! Flag command type (short).
+  character, parameter :: CMD_TYPE_FLAG_L = "F"
+    !! Flag command type (long).
+  character, parameter :: CMD_TYPE_KEYVAL_S = "k"
+    !! Key-value command type (short).
+  character, parameter :: CMD_TYPE_KEYVAL_L = "K" 
+    !! Key-value command type (long).
+
+  character, parameter :: VOID_CHAR = achar(0)
+    !! Placeholder value.
+
+  ! Value for flags.
+  character, parameter :: FLAG_TOGGLED = "T"
+    !! Toggled state of flag command.
+
+  ! Command Identifiers.
+  character(len=1), parameter :: SHORT_CMD_ID = "-"
+    !! Identifier for short commands.
+  character(len=2), parameter :: LONG_CMD_ID = "--"
+  !! Identifier for long commands.
+
+  type :: CmdArgParser
+    private
+    type(HashTable) :: cmdTypeTable
+      !! Table of commands and their corresponding types.
+    type(HashTable) :: cmdValueTable
+      !! Table of commands and their corresponding values obtained from
+      !! command-line arguments.
+    type(HashTable) :: cmdAliasTable
+      !! Table of aliases of commands.
+    type(HashTable) :: cmdUsageTable
+      !! Table of usage message for commands.
+    logical :: isInit = .false.
+      !! Initialization state.
+    integer :: cmdCount = 0
+      !! Number of defined commands defined in this `CmdArgParser` object.
+  contains
+    procedure :: init => cmdargparser_init
+      !! Initialize this `CmdArgParser` object.
+    procedure :: setCmd => cmdargparser_setCmd
+      !! Set a command, its type and its usage text. An alias can be optionally 
+      !! be set.
+    procedure :: readCmdArgs => cmdargparser_readCmdArgs
+      !! Read and parse the command-line arguments.
+    procedure :: printHelp => cmdargparser_printhelp
+      !! Print the help message.
+    procedure :: getCmdValue => cmdargparser_getCmdValue
+      !! Get the value of the given command.
+    procedure :: free => cmdargparser_free
+      !! Free allocated attributes of this `CmdArgParser` object.
+  end type
+
+  ! Interface for submodule procedures.
+  ! -------------------------------------------------------------------------- !
+  interface
+    module subroutine parseCmdArgs(parserObj)
+      class(CmdArgParser), intent(inout) :: parserObj
+        !! `CmdArgParser` object.
+    end subroutine parseCmdArgs
+  end interface
+
+  ! Public elements in this module.
+  ! -------------------------------------------------------------------------- !
+  public :: CmdArgParser
+
+  public :: CMD_TYPE_FLAG_S
+  public :: CMD_TYPE_FLAG_L
+  public :: CMD_TYPE_KEYVAL_S
+  public :: CMD_TYPE_KEYVAL_L
+
+  public :: FLAG_TOGGLED
+contains
+
+
+  ! -------------------------------------------------------------------------- !
+  ! SUBROUTINE: cmdargparser_init
+  !>  Initialize the `CmdArgParser` object `self`.
+  ! -------------------------------------------------------------------------- !
+  subroutine cmdargparser_init(self)
+    class(CmdArgParser), target, intent(inout) :: self
+      !! `CmdArgParser` object to be initialized. We impose that `self` is a
+      !! target for pointers for its hash tables to be iteratable by
+      !! hash table iterators.
+
+    ! Initialize the hash table attributes.
+    call self % cmdTypeTable % init()
+    call self % cmdValueTable % init()
+    call self % cmdAliasTable % init()
+    call self % cmdUsageTable % init()
+    self % isInit = .true.
+
+    ! Add the command for print the help message.
+    call self % setCmd("h", CMD_TYPE_FLAG_S, "Show this help message.", &
+        "help", CMD_TYPE_FLAG_L)
+  end subroutine cmdargparser_init
+
+
+  ! -------------------------------------------------------------------------- !
+  ! SUBROUTINE: cmdargparser_setCmd
+  !>  Set a command, its type and its usage text. An alias can be optionally
+  !!  be set.
+  ! -------------------------------------------------------------------------- !
+  subroutine cmdargparser_setCmd(self, cmdName, cmdType, cmdUsage, cmdAlias, &
+    cmdAliasType)
+    class(CmdArgParser),        intent(inout) :: self
+      !! `CmdArgParser` object to be initialized.
+    character(len=*),           intent(in)    :: cmdName
+      !! Name of the command.
+    character(len=*),           intent(in)    :: cmdType
+      !! Type of the command `cmdName`.
+    character(len=*),           intent(in)    :: cmdUsage
+      !! Usage message for the command `cmdName`.
+    character(len=*), optional, intent(in)    :: cmdAlias
+      !! Name of the alias for the command `cmdName`.
+    character(len=*), optional, intent(in)    :: cmdAliasType
+      !! Type of the command `cmdALias`.
+
+    character :: dummyChar 
+    integer   :: getStat
+  
+    ! Check initialization state.
+    if (.not. self % isInit) then
+      call raiseError( &
+        "Cannot set commands. `CmdArgParser` is not yet initialized." &
+        )
+    end if
+
+    if (.not. isValidCmdType(cmdType)) then
+      call raiseError("Cannot set a command. Invalid type of command.")
+    end if
+
+    ! Check if `cmdName` is already defined.
+    dummyChar = self % cmdTypeTable % get(cmdName, getStat)
+    if (getStat == HSHTBLE_STAT_OK) then
+      call raiseWarning( &
+        "Overwriting the command '(" &
+        // SHORT_CMD_ID // "/" // LONG_CMD_ID // ")" // trim(cmdName) // "'" &
+        )
+    end if
+
+    call self % cmdTypeTable % set(cmdName, cmdType)
+    call self % cmdUsageTable % set(cmdName, cmdUsage)
+    call self % cmdValueTable % set(cmdName, VOID_CHAR)
+
+    ! Increment the number of commands.
+    self % cmdCount = self % cmdCount + 1
+
+    ! Include an alias for `cmdName`.
+    if (present(cmdAlias) .or. present(cmdAliasType)) then
+      if (.not. present(cmdAlias) .and. present(cmdAliasType)) then
+        call raiseError( &
+          "Cannot set command. " // &
+          "'cmdAlias' and 'cmdAliasType' must be both present or absent." &
+          )
+      end if
+
+      ! Check all invalid pair of values for `cmdAliasType` and `cmdType`.
+      select case(cmdAliasType)
+        case(CMD_TYPE_FLAG_L)
+          if (cmdType /= CMD_TYPE_FLAG_S) then
+            call raiseError( &
+              "Invalid type for alias command. " // &
+              "Long flag commands must only alias short flag commands." &
+              )
+          end if
+
+        case(CMD_TYPE_FLAG_S)
+          if (cmdType /= CMD_TYPE_FLAG_S) then
+            call raiseError( &
+              "Invalid type for alias command. " // &
+              "Short flag commands must only alias long flag commands." &
+              )
+          end if
+
+        case(CMD_TYPE_KEYVAL_L)
+          if (cmdType /= CMD_TYPE_KEYVAL_S) then
+            call raiseError( &
+              "Invalid type for alias command. " // &
+              "Long key-value commands must "   // &
+              "only alias short key-value commands." &
+              )
+          end if
+
+        case(CMD_TYPE_KEYVAL_S)
+          if (cmdType /= CMD_TYPE_KEYVAL_L) then
+            call raiseError( &
+              "Invalid type for alias command. " // &
+              "Short key-value commands must "   // &
+              "only alias long key-value commands." &
+              )
+          end if
+
+        case default
+          call raiseError("Cannot set an alias for a command. Invalid type.")
+      end select
+
+      ! Check if `cmdAlias` is already defined.
+      dummyChar = self % cmdTypeTable % get(cmdAlias, getStat)
+      if (getStat == HSHTBLE_STAT_OK) then
+        call raiseWarning("Overwriting the command '"// trim(cmdName) // "'.")
+      end if
+
+      ! Check if `cmdAlias` is already defined.
+      dummyChar = self % cmdTypeTable % get(cmdAlias, getStat)
+      if (getStat == HSHTBLE_STAT_OK) then
+        call raiseWarning( &
+          "Overwriting the command '(" &
+          // SHORT_CMD_ID // "/" // LONG_CMD_ID // ")" // trim(cmdAlias) // &
+          "' as alias for '" // trim(cmdName) // "'." &
+          )
+      end if
+
+      ! Finally assign the alias.
+      call self % cmdAliasTable % set(cmdName, cmdAlias)
+      call self % cmdAliasTable % set(cmdAlias, cmdName)
+
+      ! Assign the alias as one of the commands.
+      call self % cmdTypeTable % set(cmdAlias, cmdAliasType)
+      call self % cmdUsageTable % set(cmdAlias, cmdUsage)
+      call self % cmdValueTable % set(cmdAlias, VOID_CHAR)
+
+      ! Increment the number of commands.
+      self % cmdCount = self % cmdCount + 1
+    end if
+  end subroutine cmdargparser_setCmd
+
+
+  ! -------------------------------------------------------------------------- !
+  ! FUNCTION: isValidCmdType
+  !>  Check if `cmdType` is a valid command type.
+  ! -------------------------------------------------------------------------- !
+  logical pure function isValidCmdType(cmdType)
+    character(len=*), intent(in) :: cmdType
+
+    isValidCmdType = (cmdType == CMD_TYPE_FLAG_L)   .or. &
+                     (cmdType == CMD_TYPE_FLAG_S)   .or. &
+                     (cmdType == CMD_TYPE_KEYVAL_L) .or. &
+                     (cmdType == CMD_TYPE_KEYVAL_S)
+  end function isValidCmdType
+
+
+  ! -------------------------------------------------------------------------- !
+  ! SUBROUTINE: cmdargparser_readCmdArgs
+  !>  Read and parse the command-line arguments.
+  ! -------------------------------------------------------------------------- !
+  subroutine cmdargparser_readCmdArgs(self)
+    class(CmdArgParser), intent(inout) :: self
+      !! `CmdArgParser` object to be modified.
+
+    ! Check initialization state of `self`.
+    if (.not. self % isInit) call raiseError("Uninitialized 'CmdArgParser'.")
+
+    call parseCmdArgs(self)
+  end subroutine cmdargparser_readCmdArgs
+
+
+  ! -------------------------------------------------------------------------- !
+  ! SUBROUTINE: cmdargparser_printhelp
+  !>  Print the help message.
+  ! -------------------------------------------------------------------------- !
+  subroutine cmdargparser_printhelp(self, progName, progDesc)
+    class(CmdArgParser), target, intent(inout) :: self
+      !! `CmdArgParser` object.
+    character(len=*),            intent(in)    :: progName
+      !! Name of the program using this module.
+    character(len=*),            intent(in)    :: progDesc
+      !! Program description using this module.
+
+
+    integer, parameter :: MAX_CMD_LEN = 20 ! Max length of command to display.
+    integer :: i, j, getStat
+
+    character(len=:), allocatable :: cmdNames(:)
+    character(len=MAX_CMD_LEN)    :: cmdAlias
+    character(len=MAX_CMD_LEN)    :: shortCmd
+    character(len=MAX_CMD_LEN)    :: longCmd
+
+
+    type(HashTable), pointer :: usageTable_ptr
+    type(HashTableIterator)  :: usageTableIter
+
+    ! Check initialization state.
+    if (.not. self % isInit) then
+      call raiseError( &
+        "Cannot print commands. `CmdArgParser` is not yet initialized." &
+        )
+    end if
+
+    ! Initialize hash table iterator.
+    usageTable_ptr => self % cmdUsageTable
+    call usageTableIter % init(usageTable_ptr)
+
+    ! Initialize array of command names.
+    allocate(character(len=MAX_CMD_LEN) :: cmdNames(self % cmdCount))
+
+    ! Get the command names.
+    do i = 1, self % cmdCount
+      cmdNames(i) = usageTableIter % getKey()
+    end do
+
+    ! Print the header of the help message.
+    print "(3a/)", "Usage: ", trim(progName), " [options]"
+    print "(a/)", trim(progDesc)
+    print "(a)", "Options:"
+
+    ! Print the commands.
+    do i = 1, self % cmdCount
+      ! Ignore removed commands.
+      if (len_trim(cmdNames(i)) == 0) cycle
+
+      ! Identify which position in the help message does `cmdNames` occupy.
+      if (isShortCmd(self, cmdNames(i))) then
+        shortCmd = appendCmdID(self, cmdNames(i))
+      else
+        longCmd = appendCmdID(self, cmdNames(i))
+      end if
+
+      ! Check if the current command name has an alias.
+      cmdAlias = self % cmdAliasTable % get(cmdNames(i), getStat)
+      if (getStat == HSHTBLE_STAT_OK) then
+
+        ! Remove the alias command from command name array.
+        do j = i, self % cmdCount
+          if (cmdNames(j) == cmdAlias) cmdNames(j) = ""
+        end do
+
+        ! Identify which position in the help message does `cmdNames` occupy.
+        if (isShortCmd(self, cmdAlias)) then
+          shortCmd = appendCmdID(self, cmdAlias)
+        else
+          longCmd = appendCmdID(self, cmdAlias)
+        end if
+      end if
+
+      print "(' ', a10, a20, a)", &
+          shortCmd, longCmd, self % cmdUsageTable % get(cmdNames(i))
+    end do
+
+    ! Free local allocated variables.
+    deallocate(cmdNames)
+  end subroutine cmdargparser_printhelp
+
+
+  ! -------------------------------------------------------------------------- !
+  ! FUNCTION: isShortCmd
+  !>  Check if `cmdName` is a short command or not.
+  ! -------------------------------------------------------------------------- !
+  logical function isShortCmd(cmdParser, cmdName)
+    class(CmdArgParser), intent(inout) :: cmdParser
+    character(len=*),    intent(in)    :: cmdName
+
+    character :: cmdType
+
+    cmdType = cmdParser % cmdTypeTable % get(cmdName)
+
+    isShortCmd = (cmdType == CMD_TYPE_FLAG_S .or.cmdType == CMD_TYPE_KEYVAL_S)
+  end function isShortCmd
+
+
+  ! -------------------------------------------------------------------------- !
+  ! FUNCTION: appendCmdID
+  !>  Append characters around command name `cmdName` depending on its type.
+  ! -------------------------------------------------------------------------- !
+  function appendCmdID(cmdParser, cmdName) result(cmd)
+    class(CmdArgParser), intent(inout) :: cmdParser
+    character(len=*),    intent(in)    :: cmdName
+
+    character(len=:),    allocatable   :: cmd
+
+    allocate(character(len=0) :: cmd)
+
+    select case(cmdParser % cmdTypeTable % get(cmdName))
+      case(CMD_TYPE_KEYVAL_L)
+        cmd = LONG_CMD_ID // trim(cmdName) // "=<val>"
+      case(CMD_TYPE_KEYVAL_S)
+        cmd = SHORT_CMD_ID // trim(cmdName) // " <val>"
+      case(CMD_TYPE_FLAG_L)
+        cmd = LONG_CMD_ID // trim(cmdName)
+      case(CMD_TYPE_FLAG_S)
+        cmd = SHORT_CMD_ID // trim(cmdName)
+      case default
+        call raiseError("Unknown internal error encountered.")
+    end select
+  end function appendCmdID
+
+
+  ! -------------------------------------------------------------------------- !
+  ! SUBROUTINE: cmdargparser_getCmdValue
+  !>  Get the value of the given command.
+  ! -------------------------------------------------------------------------- !
+  function cmdargparser_getCmdValue(self, cmdName, status) result(cmdValue)
+    class(CmdArgParser), intent(inout) :: self
+      !! `CmdArgParser` object to be searched.
+    character(len=*),    intent(in)    :: cmdName
+      !! Name of the command whose value is to be searched.
+    integer, optional,   intent(out)   :: status
+    !! Get status.
+
+    character(len=:), allocatable :: cmdValue
+      !! Output value.
+
+    character :: cmdType
+    integer   :: localStat
+
+    ! Initialize output.
+    allocate(character(len=0) :: cmdValue)
+
+    ! Get the value.
+    cmdValue = self % cmdValueTable % get(cmdName, localStat)
+    if (localStat /= HSHTBLE_STAT_OK) then
+      ! NOTE: If `cmdName` does not exist, it should not even have an alias.
+      !       So here, we don't get the alias for `cmdName`.
+      call raiseError("Unknown command name '" // trim(cmdName) // "'.")
+    end if
+
+    cmdType = self % cmdTypeTable % get(cmdName, localStat)
+    if (localStat /= HSHTBLE_STAT_OK) then
+      call raiseError("Unknown internal error encountered.")
+    end if
+
+    ! Check if a value is obtained for the command `cmdName`.
+    ! NOTE: No check needed for flag commands. No value means 'untoggled'.
+    if (cmdValue == VOID_CHAR .and. &
+        (cmdType /= CMD_TYPE_FLAG_S .and. cmdType /= CMD_TYPE_FLAG_L)) then
+  
+      if (present(status)) then
+        status = 1
+      else
+        call raiseError( &
+          "No value obtained for the command '(" &
+          // SHORT_CMD_ID // "/" // LONG_CMD_ID // ")" // trim(cmdName) // "'."&
+          )
+      end if
+    end if
+  end function cmdargparser_getCmdValue
+
+
+  ! -------------------------------------------------------------------------- !
+  ! SUBROUTINE: cmdargparser_free
+  !>  Free allocated attributes of the `CmdArgParser` object `self`.
+  ! -------------------------------------------------------------------------- !
+  subroutine cmdargparser_free(self)
+    class(CmdArgParser), intent(inout) :: self
+      !! `CmdArgParser` object whose allocated attributes are to be freed.
+  
+    ! Free all hash table attributes.
+    call self % cmdTypeTable % free()
+    call self % cmdValueTable % free()
+    call self % cmdAliasTable % free()
+    call self % cmdUsageTable % free()
+
+    ! Reset attributes to their initial values.
+    self % cmdCount = 0
+    self % isInit = .false.
+  end subroutine cmdargparser_free
+end module CmdArgParserType
---
+++ src/smod_CmdParser_parserprocs.f90
@@ -0,0 +1,259 @@
+submodule (CmdArgParserType) parserProcedures
+  implicit none
+  
+  integer, parameter :: MAX_ARG_LEN = 64
+    !! Maximum length of command-line argument.
+contains
+
+
+  ! -------------------------------------------------------------------------- !
+  ! SUBROUTINE: parseCmdArgs
+  !>  Parse command arguments.
+  ! -------------------------------------------------------------------------- !
+  subroutine parseCmdArgs(parserObj)
+    class(CmdArgParser), intent(inout) :: parserObj
+      !! `CmdArgParser` object.
+
+    character(len=MAX_ARG_LEN) :: rawChar   ! Raw command argument.
+    character(len=:), allocatable :: cmdArg ! Cleaned command argument.
+
+    integer :: cmdIdx     ! Command-line argument position/index
+    integer :: getCmdStat ! Status of `get_command_argument` routine.
+
+    ! Initialize local variables.
+    allocate(character(len=0) :: cmdArg)
+    cmdIdx = 1
+
+    do
+      call get_command_argument(number=cmdIdx, value=rawChar, status=getCmdStat)
+      
+      ! Exit condition. Positive value means that retrieval failed.
+      if (getCmdStat > 0) exit
+
+      ! Trim extra spaces.
+      cmdArg = trim(rawChar)
+
+      ! Parse for long commands.
+      if (len(cmdArg) > 2) then
+        if (cmdArg(1:2) == LONG_CMD_ID) then
+          call parseLongCmds(parserObj, cmdArg(3:))
+
+          ! Get to the next command argumnet.
+          cmdIdx = cmdIdx + 1
+          cycle
+        end if
+      end if
+
+      ! Parse for short commands.
+      if (len(cmdArg) > 1) then
+        if (cmdArg(1:1) == SHORT_CMD_ID) then
+          call parseShortCmds(parserObj, cmdIdx, cmdArg(2:))
+
+          ! Get to the next command argumnet.
+          cmdIdx = cmdIdx + 1
+          cycle
+        end if
+      end if
+
+      call raiseError("Invalid command '" // cmdArg // "'.")
+    end do
+  end subroutine parseCmdArgs
+
+
+  ! -------------------------------------------------------------------------- !
+  ! SUBROUTINE: parseShortCmds
+  !>  Parse the command argument `cmdArg` as a short command, i.e. command
+  !!  starting with a single '-' character.
+  ! -------------------------------------------------------------------------- !
+  subroutine parseShortCmds(parserObj, cmdIdx, cmdArg)
+    type(CmdArgParser),  intent(inout) :: parserObj
+      !! `CmdArgParser` object to be modified.
+    integer,             intent(inout) :: cmdIdx  
+      !! Index for indexing command arguments.
+      !! NOTE: `cmdIdx` may mutate in this routine.
+    character(len=*),    intent(in)    :: cmdArg
+      !! Command argument to be parsed. This should not contain the '-'
+      !! identifier at the beginning of command arguments.
+
+    character(len=MAX_ARG_LEN) :: KVCmdValue ! Value for the key-value
+    character :: currChar, cmdType
+    integer :: i, getStat
+
+    do i = 1, len(cmdArg)
+      currChar = cmdArg(i:i)
+
+      ! Get the type of the current command.
+      cmdType = parserObj % cmdTypeTable % get(currChar, getStat)
+      if (getStat /= HSHTBLE_STAT_OK) &
+        call raiseError("Invalid command '" // SHORT_CMD_ID // currChar // "'.")
+
+      ! Set the value for the obtained command.
+      select case(cmdType)
+        case(CMD_TYPE_FLAG_S)
+          call parserObj % cmdValueTable % set(cmdArg, FLAG_TOGGLED)
+          call setValToAlias(parserObj, currChar, FLAG_TOGGLED)
+
+        case(CMD_TYPE_KEYVAL_S)
+          ! Get the next command argument.
+          cmdIdx = cmdIdx + 1
+          call get_command_argument(number=cmdIdx, value=KVCmdValue, &
+            status=getStat)
+
+          if (getStat > 0) then
+            call raiseError( &
+              "No value obtained for the command '" &
+              // SHORT_CMD_ID // currChar // "'." &
+              )
+          end if
+
+          ! Finally set value to the key-value command.
+          call parserObj % cmdValueTable % set(cmdArg, trim(KVCmdValue))
+          call setValToAlias(parserObj, currChar, KVCmdValue)
+
+        case(CMD_TYPE_FLAG_L)
+          call raiseError("Invalid command. Did you mean '" &
+            // LONG_CMD_ID // currChar // "'?"&
+            )
+
+        case(CMD_TYPE_KEYVAL_L)
+          call raiseError("Invalid command. Did you mean '" &
+            // LONG_CMD_ID // currChar // "=<value>'?"&
+            )
+
+        case default
+          call raiseError( &
+            "Internal error. Unknown type for '" &
+            // SHORT_CMD_ID // currChar // "'" &
+            )
+      end select
+    end do
+  end subroutine parseShortCmds
+
+
+  ! -------------------------------------------------------------------------- !
+  ! SUBROUTINE: parseLongCmds
+  !>  Parse the command argument `cmdArg` as a long command, i.e. command
+  !!  starting with two '-' characters.
+  ! -------------------------------------------------------------------------- !
+  subroutine parseLongCmds(parserObj, cmdArg)
+    class(CmdArgParser), intent(inout) :: parserObj
+      !! `CmdArgParser` object to be modified.
+    character(len=*),    intent(in)    :: cmdArg
+      !! Command argument to be parsed. This should not contain the '--'
+      !! identifier at the beginning of command arguments.
+
+    ! Key-value separator.
+    character, parameter :: KEYVAL_SEP = "="
+
+    ! Temporary character for holding either key or value substring.
+    character(len=:), allocatable :: keyChar, valueChar
+
+    character :: currChar, cmdType
+    logical   :: isReadingKey
+    integer   :: i, getStat
+
+    ! Initialize local variables.
+    allocate(character(len=0) :: keyChar)
+    allocate(character(len=0) :: valueChar)
+    isReadingKey = .true.
+
+    ! Read command argument as if it is a key-value command; the methods
+    ! for key-value and flag commands are similar.
+    do i = 1, len(trim(cmdArg))
+      currChar = cmdArg(i:i)
+
+      ! Switch from reading key to reading value.
+      if (currChar == KEYVAL_SEP) then
+        isReadingKey = .false.
+        cycle
+      end if
+
+      if (isReadingKey) then
+        keyChar = keyChar // currChar
+      else
+        valueChar = valueChar // currChar
+      end if
+    end do
+
+    ! Check if a key was indeed obtained.
+    if (len(keyChar) == 0) then
+      call raiseError("Missing key")
+    end if
+
+    ! Get the type of the obtained key.
+    cmdType = parserObj % cmdTypeTable % get(keyChar, getStat)
+    if (getStat /= HSHTBLE_STAT_OK) then
+      call raiseError( &
+        "Invalid command '" // LONG_CMD_ID //  keyChar // "'." &
+        )
+    end if
+
+    ! Finally set the value for the obtained command.
+    select case(cmdType)
+      case(CMD_TYPE_FLAG_L)
+        ! Check if the flag command is treated as a key-value command.
+        if (len(valueChar) > 0) then
+          call raiseError( &
+            "The command '" // LONG_CMD_ID // keyChar // &
+            "' does not accept a value." &
+            )
+        end if
+
+        call parserObj % cmdValueTable % set(keyChar, FLAG_TOGGLED)
+        call setValToAlias(parserObj, keyChar, FLAG_TOGGLED)
+
+      case(CMD_TYPE_KEYVAL_L)
+        if (isReadingKey .or. len(valueChar) == 0) then
+          call raiseError( &
+            "Missing value for the command '" // LONG_CMD_ID // keyChar // &
+            "'." &
+            )
+        end if
+
+        call parserObj % cmdValueTable % set(keyChar, valueChar)
+        call setValToAlias(parserObj, keyChar, valueChar)
+
+      case(CMD_TYPE_FLAG_S, CMD_TYPE_KEYVAL_S)
+        call raiseError( &
+          "Invalid command. Did you mean '" // SHORT_CMD_ID // keyChar // "'?" &
+          )
+
+      case default
+        call raiseError( &
+          "Internal error. Unknown type for '" // keyChar // "'" &
+          )
+    end select
+
+    deallocate(keyChar)
+    deallocate(valueChar)
+  end subroutine parseLongCmds
+
+
+  ! -------------------------------------------------------------------------- !
+  ! SUBROUTINE: setValToAlias
+  !>  Set the value of the alias of the command `cmdName`.
+  ! -------------------------------------------------------------------------- !
+  subroutine setValToAlias(parserObj, cmdName, valueChar)
+    type(CmdArgParser), intent(inout) :: parserObj
+      !! `CmdArgParser` object to be modified.
+    character(len=*),   intent(in)    :: cmdName
+      !! Command name.
+    character(len=*),   intent(in)    :: valueChar
+      !! Value for the command `cmdName`.
+
+    character(len=:), allocatable :: cmdAlias
+    integer :: getStat
+
+    ! Initialize local variable.
+    allocate(character(len=0) :: cmdAlias)
+
+    cmdAlias = parserObj % cmdAliasTable % get(cmdName, getStat)
+    if (getStat /= HSHTBLE_STAT_OK) return
+  
+    ! Set value of `cmdName` to its alias `cmdAlias`.
+    call parserObj % cmdValueTable % set(cmdAlias, valueChar, getStat)
+    if (getStat /= HSHTBLE_STAT_OK) then
+      call raiseError("Unknown internal error encountered.")
+    end if
+  end subroutine setValToAlias
+end submodule parserProcedures
