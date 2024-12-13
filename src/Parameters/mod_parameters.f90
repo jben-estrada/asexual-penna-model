ace1c05768ea26423b76cbd090dcdb8a8b93512e
John Benedick Estrada <ejohnbenedick@yahoo.com>
2024-12-14 02:48:14 +0800

    Update the version to `2024.12.1`

--- src/Parameters/mod_parameters.f90
+++ src/Parameters/mod_parameters.f90
@@ -109,7 +109,7 @@ module Parameters
       "Asexual Penna model: A computer model for biological aging based " // &
       "on mutation accumulation."
     !! Description of the program
-  character(len=*), parameter :: PROG_VERSION = "v2024.12.0"
+  character(len=*), parameter :: PROG_VERSION = "v2024.12.1"
     !! Program version.
     !! This is based on the year and month the program is released.
 
